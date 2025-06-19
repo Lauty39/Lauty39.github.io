63c66d22e28409a43b13f1d9489335869c810b85
Lauty39 <martinezlautaro000@gmail.com>
2025-06-19 18:42:42 -0300

    mas cambios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://lauty39-github-io.onrender.com';
 
 export default API_BASE_URL; 
